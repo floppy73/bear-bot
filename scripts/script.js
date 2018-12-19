2af45e777c32369b53e3018d703c7730fd1644b3
floppy73 <seisei_doda@yahoo.co.jp>
2018-12-19 04:24:34 +0000

    お礼への返答

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,29 @@
 'use strict';
 module.exports = (robot) => {
+    // 話しかける感じの言葉への返答
     robot.hear(/助けて|たすけて|聞いて|きいて|ねえ/i, (msg) => {
         msg.send('なに？');
     });
 
+    // お礼する感じの言葉への返答
+    robot.hear(/ありがとう|助かった|たすかった/i, (msg) => {
+        const messages = [
+            'どういたしまして～',
+            'がんばってね',
+            'よかった〜'
+        ];
+        const message = messages[Math.floor(Math.random() * messages.length)];
+        msg.send(message);
+    });
+
+    // 上記以外のメッセージに対して相槌を打つ
     robot.catchAll((msg) => {
-        msg.send('うんうん');
+        const messages = [
+            'うん',
+            'うんうん',
+            'ほう'
+        ];
+        const message = messages[Math.floor(Math.random() * messages.length)];
+        msg.send(message);
     });
 };
